85eb8d3257866a73ffd23214e3eba91a6bde3c70
Corey Frenette <corey@narwhunderful.com>
2015-07-05 20:35:37 -0300

    Positive integer

---
+++ src/PositiveInteger.php
@@ -0,0 +1,24 @@
+<?php namespace BapCat\Values;
+
+use BapCat\Interfaces\Values\Value;
+
+use InvalidArgumentException;
+
+class PositiveInteger extends Value {
+  private $raw;
+  
+  public function __construct($positive) {
+    $this->validate($positive);
+    $this->raw = $positive;
+  }
+  
+  private function validate($positive) {
+    if(filter_var($inputVal, FILTER_VALIDATE_INT, ['options' => ['min_range' => 0]]) === false) {
+      throw new InvalidArgumentException("Expected a positive integer, but got [$positive] instead");
+    }
+  }
+  
+  protected function getRaw() {
+    return $this->raw;
+  }
+}
