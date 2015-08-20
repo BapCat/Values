ab429eaa4426440acb9b2a1af72ac42a45943f19
Corey Frenette <corey@narwhunderful.com>
2015-08-20 16:04:20 -0300

    Add URL value object

---
+++ src/Url.php
@@ -0,0 +1,59 @@
+<?php namespace BapCat\Values;
+
+use BapCat\Interfaces\Values\Value;
+
+use InvalidArgumentException;
+
+/**
+ * Represents a URL
+ *
+ * @author    Corey Frenette
+ * @copyright Copyright (c) 2015, BapCat
+ */
+class Url extends Value {
+  /**
+   * @var string  The raw URL
+   */
+  private $raw;
+
+  /**
+   * Constructor
+   *
+   * @param  string  $url  The raw URL to wrap
+   */
+  public function __construct($url) {
+    $this->validate($url);
+    $this->raw = $url;
+  }
+
+  /**
+   * Ensures the URL passed in is valid
+   * 
+   * @throws  InvalidArgumentException  If the value is not a valid URL
+   * 
+   * @param  string  $url  The value to validate
+   */
+  private function validate($url) {
+    if(filter_var($url, FILTER_VALIDATE_URL) === false) {
+      throw new InvalidArgumentException("Expected URL, but got [$url] instead");
+    }
+  }
+  
+  /**
+   * Converts this object to a string
+   * 
+   * @return  string  A string representation of this object
+   */
+  public function __toString() {
+      return $this->raw;
+  }
+  
+  /**
+   * Gets the raw value this object wraps
+   * 
+   * @return  string  The raw value this object wraps
+   */
+  protected function getRaw() {
+    return $this->raw;
+  }
+}
---
+++ tests/UrlTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use BapCat\Values\Url;
+
+class AzorusRedirectUrlTest extends PHPUnit_Framework_TestCase {
+  protected $url;
+  
+  protected function setUp() {
+    $this->url = 'http://bapcat.com';
+  }
+  
+  public function testMalformedUrl() {
+    $this->setExpectedException(InvalidArgumentException::class);
+    new Url('http://:80');
+  }
+  
+  public function testNoScheme() {
+    $this->setExpectedException(InvalidArgumentException::class);
+    new Url('bapcat.com');
+  }
+  
+  public function testNoHost() {
+    $this->setExpectedException(InvalidArgumentException::class);
+    new Url('http:');
+  }
+  
+  public function testToString() {
+    $url = new Url($this->url);
+    $this->assertEquals($this->url, (string)$url);
+  }
+  
+  public function testRaw() {
+    $url = new Url($this->url);
+    $this->assertEquals($this->url, $url->raw);
+  }
+}
